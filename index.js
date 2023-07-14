89833313cda7f726a9dbe056b2a3dc171703679c
Kristin Ponsonby <kristinponsonby@gmail.com>
2023-07-14 18:35:47 -0400

    add media queries

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const form = document.querySelector('form')
-form.addEventListener('submit', (e) => {
-    
-})
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card-1");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("ive been clicked!")
+    card_1.classList.add("hide");
+    card_2.classList.remove("hide");
+});
